9fcf14e485f99f7c6305249076b84716a36eb87c
Samuel Rodriguez-Sevilla <laparca@laparca.es>
2011-01-25 23:58:24 +0100

    [improved] Reordered tables
    * There are two tables, history and stats, that they can be allocated at memory.

--- v3/controller/app/config/database.sql
+++ v3/controller/app/config/database.sql
@@ -12,19 +12,18 @@ CREATE TABLE IF NOT EXISTS `cracks` (
 	`disposition` VARCHAR(6)   NOT NULL,
 	`collision`   VARCHAR(255) NOT NULL,
 	`updated`     INT(11)      NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS `stats` (
+CREATE TABLE IF NOT EXISTS `hashes` (
 	`id`          INT(10)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`updated`     INT(11)      NOT NULL,
-	`device`      VARCHAR(255) NOT NULL,
-	`time`        INT(11)      NOT NULL,
-	`speed`       FLOAT        NOT NULL
+	`active`      INT(11)      NOT NULL,
+	`crack_id`    INT(10)      NOT NULL,
+	`hash`        TEXT         NOT NULL,
+	`collision`   TEXT         NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 CREATE TABLE IF NOT EXISTS `work_units` (
 	`id`          INT(10)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
---	`crack`       INT(11)      NOT NULL,
 	`crack_id`    INT(10)      NOT NULL,
 	`hostname`    VARCHAR(255) NOT NULL,
 	`devtype`     VARCHAR(16)  NOT NULL,
@@ -33,7 +32,7 @@ CREATE TABLE IF NOT EXISTS `work_units` (
 	`end`         VARCHAR(255) NOT NULL,
 	`started`     INT(11)      NOT NULL,
 	`expiration`  INT(11)      NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci AUTO_INCREMENT=1;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- A memory table must be better than a MyISAM table. History has lots of access
 -- but no essential data
@@ -45,11 +44,10 @@ CREATE TABLE IF NOT EXISTS `history` (
 
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
-CREATE TABLE IF NOT EXISTS `hashes` (
+CREATE TABLE IF NOT EXISTS `stats` (
 	`id`          INT(10)      NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	`active`      INT(11)      NOT NULL,
-	`crack_id`    INT(10)      NOT NULL,
---	`crack`       INT(11)      NOT NULL,
-	`hash`        TEXT         NOT NULL,
-	`collision`   TEXT         NOT NULL
+	`updated`     INT(11)      NOT NULL,
+	`device`      VARCHAR(255) NOT NULL,
+	`time`        INT(11)      NOT NULL,
+	`speed`       FLOAT        NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
